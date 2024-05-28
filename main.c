f1d35b846f30a0f4fe426c8b8f5282edad9832ca
Ankit saini <ankitsaini0416@gmail.com>
2024-05-28 15:03:35 +0530

    this is my c file`

---
+++ main.c
@@ -0,0 +1,31 @@
+// #include <stdio.h>
+// int main(){
+//     printf("Hello world");
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     printf("Vasu, Aryan, Rahul, Khushi, Bhanu, Potter");
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     printf("Banana, Apple, Strawberry, Avocado, Pineapple, Watermelon ");
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     printf("Switzerland, Finland, USA, China, UAE ");
+//     return 0;
+// }
+
+#include <stdio.h>
+int
+main ()
+{
+  printf ("Mumbai, Delhi, Jaipur, Chennai, Bangalore, Hyderabad, Ahmedabad");
+  return 0;
+}
